14a420a1ada8f772c1f0be9eb631b85d71312486
Aly Mamdouh <aly.momdouh@gmail.com>
2026-08-06 14:46:09 +0300

    add main project or example for ms project

---
+++ MS Unit tests/MSUnitTest/BasicMaths/BasicMath.cs
@@ -0,0 +1,25 @@
+﻿namespace BasicMaths
+{
+    public class BasicMath
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Substract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            // To trace error while testing, writing + operator instead of * operator.
+            //return num1 + num2;
+            // the correct one is 
+            return num1 * num2;
+        }
+    }
+}
